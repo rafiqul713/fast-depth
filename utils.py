3b755e059165ac0c7b56bec2e53f33eb4e5fc633
Rafiqul Islam <rafiqul.islam@s2018.tu-chemnitz.de>
2020-11-18 09:30:09 +0100

    Add save_checkpoint method to store checkpoint

--- utils.py
+++ utils.py
@@ -89,3 +89,14 @@ def get_output_directory(args):
             args.arch, args.decoder, args.criterion, args.lr, args.batch_size, \
             args.pretrained))
     return output_directory
+
+def save_checkpoint(state, is_best, epoch, output_directory):
+    checkpoint_filename = os.path.join(output_directory, 'checkpoint-' + str(epoch) + '.pth.tar')
+    torch.save(state, checkpoint_filename)
+    if is_best:
+        best_filename = os.path.join(output_directory, 'model_best.pth.tar')
+        shutil.copyfile(checkpoint_filename, best_filename)
+    if epoch > 0:
+        prev_checkpoint_filename = os.path.join(output_directory, 'checkpoint-' + str(epoch-1) + '.pth.tar')
+        if os.path.exists(prev_checkpoint_filename):
+            os.remove(prev_checkpoint_filename)
