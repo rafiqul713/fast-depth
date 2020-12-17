afbd99cde2ea1fce57122c21e336d0dae2485687
Rafiqul Islam <rafiqul.islam@s2018.tu-chemnitz.de>
2020-12-17 19:43:30 +0100

    Add configuration file

---
+++ configuration_file.py
@@ -0,0 +1,3 @@
+mobilenet_path="/home/rafiqul/Documents/Thesis/Code/GitHub/masters_thesis/Fast Depth Previous Code/mobilnet/model_best.pth.tar"
+GPU=False
+datasets_path="/media/rafiqul/D662570A6256EF2D/Datasets"
